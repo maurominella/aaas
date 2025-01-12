320cfee6f9d15873e62de7145df454399003a819
Mauro Minella <mauro.minella@mngenv789911.onmicrosoft.com>
2025-01-12 21:40:12 +0100

    aaas

---
+++ csconsoleagent/Program.cs
@@ -0,0 +1,10 @@
+﻿#nullable disable
+
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Azure.Core.TestFramework;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
